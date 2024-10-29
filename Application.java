37aa31dc2545b0913ece347404fc2518b8cfe77f
sindhubomma <sindhubomma2001@gmail.com>
2024-10-29 09:49:18 +0530

    feature202 changes

--- Application.java
+++ Application.java
@@ -20,4 +20,27 @@ public class WriteToFile
 		{	System.out.println("An error occured while trying to write to the file");
 		}
 	}
+#feature202 changes for ethan for Reports module
+public class MyClass {
+
+    // Public field
+    public int publicField = 10;
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+    }
+}
 }
